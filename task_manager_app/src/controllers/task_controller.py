4df794fcc55560c40bab180991a0533277b8cd75
jakethemich <75705725+jakethemich@users.noreply.github.com>
2026-03-02 20:17:07 -0800

    Rename list_task to list_tasks for consistency

--- task_manager_app/src/controllers/task_controller.py
+++ task_manager_app/src/controllers/task_controller.py
@@ -3,7 +3,7 @@ from  models import task_manager
 def add_task(title, description, due_date, priority):
     return task_manager.add_task(title, description, due_date, priority)
 
-def list_task():
+def list_tasks():
     return task_manager.list_tasks()
 
 def complete_task(task_id):
