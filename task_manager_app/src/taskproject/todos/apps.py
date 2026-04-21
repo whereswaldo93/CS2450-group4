c6d06f756a2a54a6246bbc842aa5d4a9ba186b66
Osvaldo Saldana <40553918+whereswaldo93@users.noreply.github.com>
2026-04-21 15:36:38 -0600

    Refactor database path handling in TodosConfig and update logger name

--- task_manager_app/src/taskproject/todos/apps.py
+++ task_manager_app/src/taskproject/todos/apps.py
@@ -1,21 +1,25 @@
 import os
 from django.apps import AppConfig
+from django.conf import settings
 import logging
 import atexit
 
-logger = logging.getLogger("todos")
+logger = logging.getLogger("taskproject")
 
 class TodosConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "todos"
 
     def ready(self):
-        # Log a message when the app is ready to confirm it's loaded
-        if not os.path.exists("db.sqlite3"):
-            logger.critical("Database not found! Please run 'py manage.py migrate' to set up the database.")
-        else:
-            logger.info("Task Manager application startup initiated. Application is ready and loaded.")
+        # The absoulte path to the database file
+        db_path = os.path.join(settings.BASE_DIR, "db.sqlite3")
 
+        # System-wide failure
+        if not os.path.exists(db_path):
+            logger.critical(f"Database `{db_path}` not found! Please run 'py manage.py migrate' to set up the database.")
+        else:
+            # Application startup
+            logger.info("Task Manager application startup initiated. Application is ready and loaded.")
         atexit.register(self.on_shutdown)
 
     def on_shutdown(self):
